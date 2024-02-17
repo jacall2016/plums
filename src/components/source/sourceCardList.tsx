f3bcc1aac2e0526ad6e87d9b4a9fbda9a424f435
jordankraude <jordankraudeTP@gmail.com>
2024-02-17 14:06:17 -0700

    applied css and made site responsive

--- src/components/source/sourceCardList.tsx
+++ src/components/source/sourceCardList.tsx
@@ -37,7 +37,7 @@ const SourceList: React.FC<Props> = ({ customKey }) => {
   const [sources, setSources] = useState<Source[]>(mockSources);
 
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 justify-center align-middle">
       {sources.map((source) => (
         <SourceCard
           key={source.id}
